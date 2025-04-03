4444c28e9c92ee18b4902d48b3a971904a9cb8d8
AnthonyRoze <anthony.rozet@gmail.com>
2025-04-02 20:58:17 -0400

    added backend

---
+++ server/HMS/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'homepage.html')
+
+def user_login(request):
+    return render(request, 'login.html')
+def appointments(request):
+    return render(request, 'appointments.html')
