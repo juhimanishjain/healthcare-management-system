137a3d51d70c01863acaabc9f26f0302ff460c55
AnthonyRoze <anthony.rozet@gmail.com>
2025-05-08 11:11:50 -0400

    debugging

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+    settings_module = 'quickstartproject.production' if 'WEBSITE_HOSTNAME' in os.environ else 'quickstartproject.settings'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
